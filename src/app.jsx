8ffb8b544da4034d9ccb703a17fb68ea8d395db2
js-RAM <swartzjoshua99@gmail.com>
2023-12-13 22:21:38 -0700

    created react login page

--- src/app.jsx
+++ src/app.jsx
@@ -3,38 +3,49 @@ import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 import { Login } from './login/login';
 import { AdventurePage } from './adventurepage/adventurepage';
 import { UserPage } from './userpage/userpage';
+import { AuthState } from './login/authState'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return (
-    <BrowserRouter>
-    <div className="body bg-light text-dark">
-        <header className="container-fluid">
-            <nav className="navbar fixed-top navbar-light">
-            <a className="navbar-brand" href="#">CS 260 RPG</a>
-                <menu>
-                    <li className = "menu-item"><NavLink className="nav-link" to='login'>Home</NavLink></li>
-                    <li className = "menu-item"><NavLink className="nav-link" to='userpage'>Character Page</NavLink></li>
-                    <li className = "menu-item"><NavLink className ="nav-link" to='adventurepage'>Your Adventure</NavLink></li>
-                </menu>
-            </nav>
-        </header>
-        <Routes>
-            <Route path='/' element={<Login />} exact />
-            <Route path='/userpage' element={<UserPage />} />
-            <Route path='/adventurepage' element={<AdventurePage />} />
-            <Route path='*' element={<NotFound />} />
-        </Routes>
-        <footer>
-            <div>
-            <span className="text-reset">Author Joshua Swartz</span>
-            <a className="text-reset" href="https://github.com/js-RAM/startup">GitHub</a>
-            </div>
-      </footer>
-    </div>
-    </BrowserRouter>
-  );
+    const [userName, setUserName] = React.useState(localStorage.getItem('userName') || '');
+    const currentAuthState = userName ? AuthState.Authenticated : AuthState.Unauthenticated;
+    const [authState, setAuthState] = React.useState(currentAuthState);
+    return (
+        <BrowserRouter>
+        <div className="body bg-light text-dark">
+            <header className="container-fluid">
+                <nav className="navbar fixed-top navbar-light">
+                <a className="navbar-brand" href="#">CS 260 RPG</a>
+                    <menu>
+                        <li className = "menu-item"><NavLink className="nav-link" to=''>Home</NavLink></li>
+                        <li className = "menu-item"><NavLink className="nav-link" to='userpage'>Character Page</NavLink></li>
+                        <li className = "menu-item"><NavLink className ="nav-link" to='adventurepage'>Your Adventure</NavLink></li>
+                    </menu>
+                </nav>
+            </header>
+            <Routes>
+                <Route path='/' element={<Login
+                    userName={userName}
+                    authState={authState}
+                    onAuthChange={(userName, authState) => {
+                    setAuthState(authState);
+                    setUserName(userName);
+                    }}
+                />} exact />
+                <Route path='/userpage' element={<UserPage />} />
+                <Route path='/adventurepage' element={<AdventurePage />} />
+                <Route path='*' element={<NotFound />} />
+            </Routes>
+            <footer>
+                <div>
+                <span className="text-reset">Author Joshua Swartz</span>
+                <a className="text-reset" href="https://github.com/js-RAM/startup">GitHub</a>
+                </div>
+        </footer>
+        </div>
+        </BrowserRouter>
+    );
 }
 
 function NotFound() {
--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,9 +1,47 @@
 import React from 'react';
 
-export function Login() {
+import { Unauthenticated } from './unauthenticated';
+import { Authenticated } from './authenticated';
+import { AuthState } from './authState';
+
+export function Login({ userName, authState, onAuthChange }) {
+  function displayQuote(data) {
+    fetch('https://api.quotable.io/random')
+      .then((response) => response.json())
+      .then((data) => {
+        const containerEl = document.querySelector('#quote');
+  
+        const quoteEl = document.createElement('p');
+        quoteEl.classList.add('quote');
+        const authorEl = document.createElement('p');
+        authorEl.classList.add('author');
+  
+        quoteEl.textContent = data.content;
+        authorEl.textContent = data.author;
+  
+        containerEl.appendChild(quoteEl);
+        containerEl.appendChild(authorEl);
+      });
+}
+
+displayQuote();
   return (
     <main className='container-fluid bg-secondary text-center'>
-      <div>login displayed here</div>
+      <div>
+        {authState !== AuthState.Unknown && <div><h1>Welcome to Your Adventure</h1><h4>Fight monsters in a turn based adventure! Level up your stats, <br></br>fight and see how many you can slay before they get the better of you</h4></div>}
+        {authState === AuthState.Authenticated && (
+          <Authenticated userName={userName} onLogout={() => onAuthChange(userName, AuthState.Unauthenticated)} />
+        )}
+        {authState === AuthState.Unauthenticated && (
+          <Unauthenticated
+            userName={userName}
+            onLogin={(loginUserName) => {
+              onAuthChange(loginUserName, AuthState.Authenticated);
+            }}
+          />
+        )}
+      </div>
+      <div id="quote"></div>
     </main>
   );
 }
