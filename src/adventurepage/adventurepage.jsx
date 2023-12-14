5410030927558b9128c10f5331f2dd9c1f79cbad
js-RAM <swartzjoshua99@gmail.com>
2023-12-13 20:58:25 -0700

    created router

---
+++ src/adventurepage/adventurepage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function AdventurePage() {
+  return (
+    <main className='container-fluid bg-secondary text-center'>
+      <div>Adventure Page displayed here</div>
+    </main>
+  );
+}
---
+++ src/userpage/userpage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function UserPage() {
+  return (
+    <main className='container-fluid bg-secondary text-center'>
+      <div>UserPage displayed here</div>
+    </main>
+  );
+}
